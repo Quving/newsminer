1989abb05ce347078faa407605addac80c71f92c
Vinh Ngu <vinh-ngu@hotmail.com>
2020-03-10 19:06:43 +0100

    Remove substrings such as '... [+230 chars]'.

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+import re
+
 from newsboxapi.newsboxapi import NewsboxApi
 
 if __name__ == '__main__':
+    # Get articles from endpoint
     newsapi = NewsboxApi()
     articles = newsapi.list_articles()
+
+    # Remove '... [+ xxx chars]' pattern from 'content'
+    for article in articles:
+        article.content = re.sub('... \[.*?\]', '', article.content)
