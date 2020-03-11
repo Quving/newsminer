8c24ec8064ad1666e99e09b47ef28f0b164f8885
Vinh Ngu <vinh-ngu@hotmail.com>
2020-03-11 21:04:30 +0100

    Fix path for tagger in entrypoint.

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,8 @@ echo "Download assets..."
 python setup.py
 
 echo "Download pre-trained german pos-tagger from minio.quving.com..."
-mkdir -p data
-curl -X GET "https://minio.quving.com/newsbox/tagger_de.pickle" --output "data/tagger_de.pickle"
+mkdir -p artifacts/tagger
+curl -X GET "https://minio.quving.com/newsbox/tagger_de.pickle" --output "artifacts/tagger/tagger_de.pickle"
 
 echo "Train and update LDA-Model."
 python train_lda.py
