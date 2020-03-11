27aaaba97e6569e66b7dabdf081585f114fdc6f1
Vinh Ngu <vinh-ngu@hotmail.com>
2020-03-11 01:19:55 +0100

    Add caching option for article list.

--- newsboxapi/newsboxapi.py
+++ newsboxapi/newsboxapi.py
@@ -1,3 +1,5 @@
+import pickle
+
 import requests
 
 from config import Config
@@ -8,26 +10,34 @@ class NewsboxApi:
     AUTH_TOKEN = Config.auth_token
     API_ENDPOINT = Config.api_endpoint
 
-    def list_articles(self):
+    def list_articles(self, from_cache):
         """
         Returns a list of all articles that is stored in the configured api endpoint. The returned list contains
          instances of the class 'Article'.
         """
-        headers = {
-            'Authorization': 'Bearer {}'.format(self.AUTH_TOKEN),
-        }
+        filename = "cache/articles.pickle"
+        if from_cache:
+            with open(filename, 'rb') as file:
+                articles = pickle.load(file)
+                return articles
+        else:
+            headers = {
+                'Authorization': 'Bearer {}'.format(self.AUTH_TOKEN),
+            }
 
-        url = '{}/news/articles/'.format(self.API_ENDPOINT)  # Initial url for page 1
+            url = '{}/news/articles/'.format(self.API_ENDPOINT)  # Initial url for page 1
 
-        list_of_articles = []
-        while url:
-            response = requests.get(url=url, headers=headers)
-            if response.status_code == 200:
-                response = response.json()
-                url = response['next']  # Overrite url to retrieve next pages.
+            list_of_articles = []
+            while url:
+                response = requests.get(url=url, headers=headers)
+                if response.status_code == 200:
+                    response = response.json()
+                    url = response['next']  # Overrite url to retrieve next pages.
 
-                articles = response['results']
-                for json in articles:
-                    articles = Article(json=json)
-                    list_of_articles.append(articles)
+                    articles = response['results']
+                    for json in articles:
+                        articles = Article(json=json)
+                        list_of_articles.append(articles)
+        with open(filename, 'wb') as file:
+            pickle.dump(list_of_articles, file)
         return list_of_articles
