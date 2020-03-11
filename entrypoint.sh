5effadd78b417b3cffb7d162850fb0481bab57fc
Vinh Ngu <vinh-ngu@hotmail.com>
2020-03-11 20:56:23 +0100

    Add setup.py.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 set -e
+
+echo "Download assets..."
+python setup.py
+
 echo "Download pre-trained german pos-tagger from minio.quving.com..."
 mkdir -p data
 curl -X GET "https://minio.quving.com/newsbox/tagger_de.pickle" --output "data/tagger_de.pickle"
